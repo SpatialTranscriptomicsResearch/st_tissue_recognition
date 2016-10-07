e76ec78218a133f316c1e5b46018dee7b2e99931
Ludvig Bergenstråhle <ludvig.bergenstrahle@scilifelab.se>
2016-10-07 12:41:33 +0200

    git hash versioning

--- python-module/setup.py
+++ python-module/setup.py
@@ -1,13 +1,21 @@
 """
 setup.py
 """
+import re
+import subprocess as sp
 import setuptools as st
 
-from layer_reg.version import __version__
+try:
+    VERSION = sp.check_output(["git", "describe"]).decode("utf8").strip()
+    # Make VERSION compliant to PEP440
+    VERSION = re.compile(r"([^\-]+?)-([^\-]+?)-(.+?)").sub(
+        r"\g<1>.post\g<2>+\g<3>", VERSION)
+except sp.CalledProcessError:
+    VERSION = "?"
 
 st.setup(
     name="tissue_recognition",
     packages=['tissue_recognition'],
-    version=__version__,
+    version=VERSION,
     zip_safe=True
 )
--- python-module/tissue_recognition/__init__.py
+++ python-module/tissue_recognition/__init__.py
@@ -1,5 +1,4 @@
 try:
-    from .version import __version__
     from .tissue_recognition import recognize_tissue
 except ImportError as ierr:
     print("Import error: %s" % ierr)
