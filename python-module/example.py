40ba1b107c4f1a91735174bc384673dd4db63434
Ludvig Bergenstråhle <ludvig.bergenstrahle@scilifelab.se>
2016-10-20 13:15:47 +0200

    example usage of python module

---
+++ python-module/example.py
@@ -0,0 +1,22 @@
+import argparse as ap
+
+import numpy as np
+from scipy.misc import imread, imshow
+
+import tissue_recognition as tr
+
+
+def main():
+    opt = ap.ArgumentParser()
+    opt.add_argument("img_file", type=str)
+    opt = opt.parse_args()
+
+    img = imread(opt.img_file)
+    msk, ann = [np.zeros(img.shape[0:2], dtype=np.uint8)]*2
+    tr.recognize_tissue(img, msk, ann)
+    bin_msk = tr.get_binary_mask(msk)
+    imshow(bin_msk)
+    tr.free(bin_msk)
+
+if __name__ == "__main__":
+    main()
